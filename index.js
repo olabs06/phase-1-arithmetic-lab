766dced6d750bcc4b0c98f7b475adc485443aef4
Simeon <olabs06@yahoo.com>
2022-06-08 10:50:52 +0100

    modifying index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+
+
+let random = Math.floor (Math.random() > 0) + 1;
+
+let num3 = 40;
+let num4 = 6;
+let mod = (num3 % num4);
+
+let max = Math.max(15, 16, 17, 18, 19, 20);
